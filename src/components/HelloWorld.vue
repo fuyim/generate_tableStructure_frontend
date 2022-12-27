14d98add741998cd2e5aba3f3fda508491a75e8b
nut~min <3271758240@qq.com>
2022-12-27 22:17:42 +0800

    添加环境变量，ant design组件支持

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
+import { message } from "ant-design-vue";
 
-defineProps<{ msg: string }>()
+const info = () => {
+  message.info("This is a normal message");
+};
 
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <a-button type="primary">Primary Button</a-button>
+  <a-button>Default Button</a-button>
+  <a-button type="dashed">Dashed Button</a-button>
+  <a-button type="text">Text Button</a-button>
+  <a-button type="link">Link Button</a-button>
+  <a-button type="primary" @click="info">Display normal message</a-button>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'; // or 'ant-design-vue/dist/antd.less'
+import { DatePicker } from 'ant-design-vue';
 
-createApp(App).mount('#app')
+const app =  createApp(App)
+app.use(DatePicker);
+
+app.mount('#app')
